f46dc3ae1733c4b4b68f6c5487d21db96cc1bbe6
mxso <dreamsong@qq.com>
2020-09-21 16:54:09 +0800

    finished should_return_3_array_when_given_a_schema

--- src/main/java/com/thoughtworks/SchemaAnalysis.java
+++ src/main/java/com/thoughtworks/SchemaAnalysis.java
@@ -3,7 +3,12 @@ package com.thoughtworks;
 public class SchemaAnalysis {
     public String[] formatShemaInput(String inputSchema) {
         String[] formatSchema = {};
-        if (null == inputSchema || "" == inputSchema) return formatSchema;
+        if (null == inputSchema || "" == inputSchema) {
+            return formatSchema;
+        }
+        else {
+            formatSchema = inputSchema.split("-");
+        }
         return formatSchema;
     }
 }
