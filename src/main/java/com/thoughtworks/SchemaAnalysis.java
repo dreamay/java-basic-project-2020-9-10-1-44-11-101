6ca719e441fb67f8523455c57ae797d0eb253d62
mxso <dreamsong@qq.com>
2020-09-21 16:50:27 +0800

    create class SchemaAnalysis

---
+++ src/main/java/com/thoughtworks/SchemaAnalysis.java
@@ -0,0 +1,9 @@
+package com.thoughtworks;
+
+public class SchemaAnalysis {
+    public String[] formatShemaInput(String inputSchema) {
+        String[] formatSchema = {};
+        if (null == inputSchema || "" == inputSchema) return formatSchema;
+        return formatSchema;
+    }
+}
