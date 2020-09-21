7bbd1c7248aeaecb2b0718fc49d9fa0e48630889
mxso <dreamsong@qq.com>
2020-09-21 17:09:19 +0800

    finished should_return_a_map_when_given_a_schema

--- src/test/java/com/thoughtworks/SchemaAnalysisTest.java
+++ src/test/java/com/thoughtworks/SchemaAnalysisTest.java
@@ -28,4 +28,16 @@ public class SchemaAnalysisTest {
         Assert.assertEquals(3,formatSchema.length);
     }
 
+    @Test
+    public void should_return_a_map_when_given_a_schema(){
+        //when
+        String inputSchema = "-l true -p 8080";
+        SchemaAnalysis schemaAnalysis = new SchemaAnalysis();
+        //given
+        schemaAnalysis.initSchema();
+        String[] formatSchema = schemaAnalysis.formatShemaInput(inputSchema);
+        Map<String,Object> objectMap = schemaAnalysis.analysisSchema(formatSchema);
+        //then
+        Assert.assertEquals(2,objectMap.size());
+    }
 }
